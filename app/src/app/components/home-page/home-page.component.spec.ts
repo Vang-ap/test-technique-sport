56267f88c5dcfc88715215d321732e138ab983b2
Plia Vang <pliaannevang.02@gmail.com>
2022-05-09 13:56:50 +0200

    modification test unit for component home-page

--- app/src/app/components/home-page/home-page.component.spec.ts
+++ app/src/app/components/home-page/home-page.component.spec.ts
@@ -1,7 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
-import { League } from 'src/app/models/league';
 import { LeagueService } from 'src/app/services/league.service';
 
 import { HomePageComponent } from './home-page.component';
@@ -9,7 +7,6 @@ import { HomePageComponent } from './home-page.component';
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
-  let leagueService: LeagueService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,25 +19,12 @@ describe('HomePageComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
-    leagueService = TestBed.inject(LeagueService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should display leagues', () => {
-    const response: League[] = [];
-
-    const spy = spyOn(leagueService, 'getLeagues').and.callFake(() => {
-      return of(response)
-    });
-
-    component.displayLeagues();
-
-    expect(spy).toHaveBeenCalled();
   });
 
 });
